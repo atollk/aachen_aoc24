84db1765d11c8b59967e646c02457028a241efda
felix <felix.widmayer@rwth-aachen.de>
2024-12-02 18:04:37 +0100

    Felix: Haskell day 2 cleanup

--- Felix/Haskell/app/Day2.hs
+++ Felix/Haskell/app/Day2.hs
@@ -1,28 +1,19 @@
 module Main where
 
-import Control.Monad (when)
-import Data.Char (digitToInt)
 import Utils
 
-parseInput = (map . map) readInt . map words
-
-solvePartOne = length . filter aux . parseInput
+maxPair :: [(Int, b)] -> Int
+maxPair = foldl aux 0
   where
-    aux list = (isValidIncreasingSequence list) || (isValidDecreasingSequence list)
+    aux acc = max acc . fst
 
-solvePartTwo = length . filter aux . parseInput
+isValidSequence :: (Ord b, Num b) => (b -> b -> Bool) -> [b] -> Bool
+isValidSequence comp seq = foldl aux True (zip seq (tail seq))
   where
-    aux list = (max (maxPair $ longestIncreasingSubsequence (\x y -> isInRange (x, y) && x < y) list) (maxPair $ longestIncreasingSubsequence (\x y -> isInRange (x, y) && x > y) list)) > length list - 2
+    aux acc pair = acc && isInRange pair && uncurry comp pair
 
+isInRange :: (Ord a, Num a) => (a, a) -> Bool
 isInRange pair = absDiff pair >= 1 && absDiff pair <= 3
-
-isValidSequence op list = foldl aux True (zip list (tail list))
-  where
-    aux acc pair = acc && isInRange pair && uncurry op pair
-
-isValidIncreasingSequence = isValidSequence (<)
-
-isValidDecreasingSequence = isValidSequence (>)
 
 -- Subproblem definition:
 -- X(i) is LIS starting at i for [i:n]
@@ -35,21 +26,32 @@ isValidDecreasingSequence = isValidSequence (>)
 -- Original problem:
 -- X(0)
 -- Time complexity:
--- O(n*n) 
+-- O(n*n)
 -- Note that = O(nlogn) solution ex, but I did not want to implement binary search in Haskell :)
-longestIncreasingSubsequence op = foldr aux []
+longestIncreasingSubsequence :: (Foldable t, Num a1, Ord a1) => (a2 -> a2 -> Bool) -> t a2 -> [(a1, a2)]
+longestIncreasingSubsequence pred = foldr getCurrLIS []
   where
-    aux val acc = (1 + impl op val acc, val) : acc
-    impl op val acc = foldl (other op val) 0 acc
-    other op ref acc (count, x) = if op ref x then max acc count else acc
+    getCurrLIS curr acc = (1 + findPrevLIS pred curr acc, curr) : acc
+    findPrevLIS pred curr = foldl (isValid pred curr) 0
+    isValid pred ref acc (count, candidate) = if pred ref candidate then max acc count else acc
 
-maxPair = foldl aux 0
+parseInput :: [String] -> [[Int]]
+parseInput = map (map readInt . words)
+
+solvePartOne :: [String] -> Int
+solvePartOne = length . filter aux . parseInput
   where
-    aux acc (x, _) = max acc x
+    aux list = isValidSequence (<) list || isValidSequence (>) list
+
+solvePartTwo :: [String] -> Int
+solvePartTwo = length . filter isValidDampenedSequence . parseInput
+  where
+    isValidDampenedSequence list = max (maxPair $ longestIncreasingSubsequence (adjacentEval (<)) list) (maxPair $ longestIncreasingSubsequence (adjacentEval (>)) list) > length list - 2
+    adjacentEval comp x y = isInRange (x, y) && comp x y
 
 main :: IO ()
 main = do
   contents <- readInput
+  print $ solvePartOne contents
   print $ solvePartTwo contents
-  -- print $ maxPair $ longestIncreasingSubsequence (<) [3, 2, 3, 4, 5]
   return ()
