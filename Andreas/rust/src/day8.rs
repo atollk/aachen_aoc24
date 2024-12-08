42545ad0b7ff760e2720d4a76a5e2f1048382bbe
Andreas Tollk?tter <andreas.tollkoetter@rwth-aachen.de>
2024-12-08 11:29:51 +0100

    Andreas day8 star1

--- Andreas/rust/src/day8.rs
+++ Andreas/rust/src/day8.rs
@@ -1,7 +1,10 @@
 #![allow(dead_code)]
 
+use itertools::Itertools;
+use std::collections::hash_set::Iter;
 use std::collections::{HashMap, HashSet};
 use std::fs;
+use std::iter::{Filter, Map, Zip};
 
 #[derive(Debug, Copy, Clone, Hash, Eq, PartialEq)]
 struct Pos(usize, usize);
@@ -17,9 +20,10 @@ fn parse_input(filename: &str) -> Input {
     let file_contents = fs::read_to_string(filename).unwrap();
     let width = file_contents.lines().next().unwrap().len();
     let height = file_contents.lines().count();
-    let chars: Vec<_> = file_contents.lines().flat_map(|line| {
-        line.chars()
-    }).collect();
+    let chars: Vec<_> = file_contents
+        .lines()
+        .flat_map(|line| line.chars())
+        .collect();
     let mut antennas = HashMap::new();
     for (i, &c) in chars.iter().enumerate() {
         let x = i % width;
@@ -29,10 +33,38 @@ fn parse_input(filename: &str) -> Input {
             positions.insert(Pos(x, y));
         }
     }
-    Input { width, height, antennas }
+    Input {
+        width,
+        height,
+        antennas,
+    }
+}
+
+fn get_antinode(lhs: Pos, rhs: Pos) -> Pos {
+    let x = (rhs.0 * 2).wrapping_sub(lhs.0);
+    let y = (rhs.1 * 2).wrapping_sub(lhs.1);
+    Pos(x, y)
+}
+
+fn antinodes<'a>(antennas: &'a HashSet<Pos>) -> impl Iterator<Item=Pos> + 'a {
+    antennas
+        .iter()
+        .cartesian_product(antennas)
+        .filter(|(lhs, rhs)| **lhs != **rhs)
+        .map(|(&lhs, &rhs)| get_antinode(lhs, rhs))
+}
+
+fn star1(input: &Input) {
+    let all_antinodes = input
+        .antennas
+        .values()
+        .flat_map(antinodes)
+        .unique()
+        .filter(|pos| pos.0 < input.width && pos.1 < input.height);
+    println!("Star 1: {}", all_antinodes.count());
 }
 
 pub(crate) fn main() {
     let input = parse_input("day8_input.txt");
-    println!("{:?}", input);
+    star1(&input);
 }
