f96c5d4b19d69ab4c52990915c5dbdc35f50479a
Andreas Tollkötter <andreas.tollkoetter@rwth-aachen.de>
2024-12-12 08:13:05 +0100

    Andreas day12 star2

--- Andreas/rust/src/day12.rs
+++ Andreas/rust/src/day12.rs
@@ -9,11 +9,41 @@ struct Pos(usize, usize);
 impl Pos {
     fn neighbours(&self) -> [Pos; 4] {
         [
-            Pos(self.0.wrapping_sub(1), self.1),
-            Pos(self.0.wrapping_add(1), self.1),
-            Pos(self.0, self.1.wrapping_sub(1)),
-            Pos(self.0, self.1.wrapping_add(1)),
+            self.mov(Direction::Down),
+            self.mov(Direction::Up),
+            self.mov(Direction::Left),
+            self.mov(Direction::Right),
         ]
+    }
+
+    fn mov(&self, direction: Direction) -> Pos {
+        match direction {
+            Direction::Up => Pos(self.0.wrapping_sub(1), self.1),
+            Direction::Down => Pos(self.0.wrapping_add(1), self.1),
+            Direction::Left => Pos(self.0, self.1.wrapping_sub(1)),
+            Direction::Right => Pos(self.0, self.1.wrapping_add(1)),
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+static ALL_DIRECTIONS: [Direction; 4] = [
+    Direction::Up,
+    Direction::Down,
+    Direction::Left,
+    Direction::Right,
+];
+
+impl Direction {
+    fn all() -> &'static [Direction; 4] {
+        &ALL_DIRECTIONS
     }
 }
 
@@ -78,31 +108,65 @@ fn find_regions(garden_plot: &GardenPlot) -> Vec<HashSet<Pos>> {
     regions
 }
 
-fn region_area(garden_plot: &GardenPlot, region: &HashSet<Pos>) -> usize {
-    region.len()
-}
-
-fn region_perimeter(garden_plot: &GardenPlot, region: &HashSet<Pos>) -> usize {
+fn region_perimeter(region: &HashSet<Pos>) -> HashSet<(Pos, Direction)> {
     region
         .iter()
-        .map(|pos| {
-            pos.neighbours()
-                .iter()
-                .filter(|n| !region.contains(n))
-                .count()
+        .flat_map(|&pos| {
+            Direction::all().iter().filter_map(move |&direction| {
+                if region.contains(&pos.mov(direction)) {
+                    None
+                } else {
+                    Some((pos, direction))
+                }
+            })
         })
-        .sum()
+        .collect()
+}
+
+fn region_sides(region: &HashSet<Pos>) -> HashSet<(Pos, Direction)> {
+    let perimeter = region_perimeter(region);
+    let mut sides = HashSet::new();
+
+    for &(pos, fence_direction) in perimeter.iter() {
+        let orthogonal_direction =
+            if fence_direction == Direction::Up || fence_direction == Direction::Down {
+                Direction::Left
+            } else {
+                Direction::Up
+            };
+        let mut sliding_pos = pos;
+        loop {
+            let next_sliding_pos = sliding_pos.mov(orthogonal_direction);
+            if !region.contains(&next_sliding_pos)
+                || !perimeter.contains(&(next_sliding_pos, fence_direction))
+            {
+                break;
+            }
+            sliding_pos = next_sliding_pos;
+        }
+        sides.insert((sliding_pos, fence_direction));
+    }
+
+    sides
 }
 
 pub(crate) fn main() {
     let garden_plot = parse_input("day12_input.txt");
     let regions = find_regions(&garden_plot);
 
-    let fence_costs: Vec<_> = regions
-        .iter()
-        .map(|region| region_area(&garden_plot, region) * region_perimeter(&garden_plot, region))
-        .collect();
+    println!(
+        "Star 1: {}",
+        regions
+            .iter()
+            .map(|region| region.len() * region_perimeter(region).len())
+            .sum::<usize>()
+    );
 
-    println!("{:?}", fence_costs);
-    println!("{}", fence_costs.iter().sum::<usize>());
+    println!(
+        "Star 2: {}",
+        regions
+            .iter()
+            .map(|region| region.len() * region_sides(region).len())
+            .sum::<usize>()
+    );
 }
