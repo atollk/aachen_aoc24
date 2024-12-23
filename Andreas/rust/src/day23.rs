9da9fe445014df403f6526dc01c4ecd4199a75a9
Andreas Tollk?tter <andreas.tollkoetter@rwth-aachen.de>
2024-12-23 13:43:19 +0100

    Andreas day23 star2

--- Andreas/rust/src/day23.rs
+++ Andreas/rust/src/day23.rs
@@ -4,6 +4,7 @@ use itertools::Itertools;
 use std::cmp::{max, min};
 use std::collections::{HashMap, HashSet};
 use std::fs;
+use std::hash::Hash;
 
 #[derive(Debug)]
 struct NetworkMap {
@@ -39,36 +40,66 @@ fn parse_input(filename: &str) -> NetworkMap {
     NetworkMap { nodes, edges }
 }
 
-pub(crate) fn main() {
-    let network_map = parse_input("day23_input.txt");
-    println!("{:?}", network_map);
-
-    let neighbours = {
-        let mut m = HashMap::new();
-        for edge in network_map.edges.iter() {
-            m.entry(edge.0).or_insert(HashSet::new()).insert(edge.1);
-            m.entry(edge.1).or_insert(HashSet::new()).insert(edge.0);
+fn is_clique(network_map: &NetworkMap, nodes: &[usize]) -> bool {
+    for i in 0..nodes.len() {
+        for j in (i + 1)..nodes.len() {
+            if !network_map.has_edge(nodes[i], nodes[j]) {
+                return false;
+            }
         }
-        m
-    };
+    }
+    true
+}
 
-    println!("{:?}", neighbours.get(&1).unwrap());
-
-    let mut cliques = HashSet::new();
-    for i in 0..network_map.nodes.len() {
-        for j in 0..network_map.nodes.len() {
-            if network_map.has_edge(i, j) {
-                let ni = neighbours.get(&i).unwrap();
-                let nj = neighbours.get(&j).unwrap();
-                for &k in ni.intersection(nj) {
-                    let mut clique = [i, j, k];
-                    clique.sort();
-                    cliques.insert(clique);
-                }
+fn cliques_of_size(
+    network_map: &NetworkMap,
+    neighbours: &[HashSet<usize>],
+    size: usize,
+) -> impl Iterator<Item=Vec<usize>> {
+    fn rec(
+        max_size: usize,
+        clique: &mut Vec<usize>,
+        neighbours: &[HashSet<usize>],
+        f: &mut dyn FnMut(&Vec<usize>) -> (),
+    ) {
+        if clique.len() == max_size {
+            f(clique);
+        } else {
+            let i = *clique.last().unwrap();
+            for &j in neighbours[i].iter().filter(|&&j| j > i) {
+                clique.push(j);
+                rec(max_size, clique, neighbours, f);
+                clique.pop();
             }
         }
     }
 
+    let mut result = Vec::new();
+    let mut clique = Vec::with_capacity(size);
+    for i in 0..network_map.nodes.len() {
+        clique.push(i);
+        rec(size, &mut clique, &neighbours, &mut |clique| {
+            if is_clique(network_map, clique) {
+                result.push(clique.clone());
+            }
+        });
+        clique.pop();
+    }
+    result.into_iter()
+}
+
+fn get_neighbours(network_map: &NetworkMap) -> Vec<HashSet<usize>> {
+    let mut m = vec![HashSet::new(); network_map.nodes.len()];
+    for edge in network_map.edges.iter() {
+        m[edge.0].insert(edge.1);
+        m[edge.1].insert(edge.0);
+    }
+    m
+}
+
+fn star1(network_map: &NetworkMap) {
+    let cliques: HashSet<_> =
+        cliques_of_size(&network_map, &get_neighbours(&network_map), 3).collect();
     let t_cliques = cliques
         .iter()
         .filter(|nodes| {
@@ -76,7 +107,53 @@ pub(crate) fn main() {
                 .iter()
                 .any(|&n| network_map.nodes[n].chars().next().unwrap() == 't')
         })
-        .map(|nodes| nodes.map(|i| &network_map.nodes[i]))
+        .map(|nodes| nodes.into_iter().map(|i| &network_map.nodes[*i]))
         .collect_vec();
-    println!("{:?}", t_cliques.len());
+    println!("Star 1: {}", t_cliques.len());
+}
+
+fn star2(network_map: &NetworkMap) {
+    let neighbours = get_neighbours(&network_map);
+
+    let mut right = 1;
+    while right < network_map.nodes.len() + 1 {
+        println!("{}", right);
+        let has_clique = cliques_of_size(&network_map, &neighbours, right)
+            .next()
+            .is_some();
+        if has_clique {
+            right *= 2;
+        } else {
+            break;
+        }
+    }
+
+    let mut left = 0;
+    while left + 1 < right {
+        println!("{} {}", left, right);
+        let mid = (left + right) / 2;
+        let has_clique = cliques_of_size(&network_map, &neighbours, mid)
+            .next()
+            .is_some();
+        if has_clique {
+            left = mid;
+        } else {
+            right = mid;
+        }
+    }
+    let largest_clique = cliques_of_size(&network_map, &neighbours, left)
+        .next()
+        .unwrap();
+    let largest_clique_names = largest_clique
+        .into_iter()
+        .map(|i| &network_map.nodes[i])
+        .sorted()
+        .collect_vec();
+    println!("Star 2: {}", largest_clique_names.into_iter().join(","));
+}
+
+pub(crate) fn main() {
+    let network_map = parse_input("day23_input.txt");
+    star1(&network_map);
+    star2(&network_map);
 }
